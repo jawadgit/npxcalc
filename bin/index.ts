f1c68da5e30985c77977f92cb8a00f5325620bc2
jawadnisar <jawwad.nissar@gmail.com>
2022-11-25 17:57:56 +0500

    calculator done

--- bin/index.ts
+++ bin/index.ts
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
-"use strict";
-exports.__esModule = true;
-var readline_sync_1 = require("readline-sync");
-function calculator() {
-    var firstNum = (0, readline_sync_1.question)('enter first number: ');
-    var checkNum = checkValidNum(firstNum);
-    var secNum = (0, readline_sync_1.question)('enter second number: ');
-    var checkSecNum = checkValidNum(secNum);
-    var operator = (0, readline_sync_1.question)('operator [+ , - , / , * ]: ');
-    var checkOperator = checkValidOperator(operator);
+
+import { question } from 'readline-sync';
+
+function calculator(): string {
+    const firstNum: string = question('enter first number: ');
+    const checkNum = checkValidNum(firstNum);
+    const secNum: string = question('enter second number: ');
+    const checkSecNum = checkValidNum(secNum);
+    const operator: string = question('operator [+ , - , / , * ]: ');
+    const checkOperator = checkValidOperator(operator);
     if (checkNum == false || checkSecNum == false || checkOperator == false) {
         console.log('invalid input exiting');
         return "invalid input exiting";
-    }
-    else {
+    } else {
         console.log(applyOperator(parseInt(firstNum), parseInt(secNum), operator));
     }
     return 'result';
 }
-function applyOperator(firstNum, secNum, operator) {
+
+
+function applyOperator(firstNum: number, secNum: number, operator: string) {
     switch (operator) {
         case '+':
             return firstNum + secNum;
@@ -30,15 +31,18 @@ function applyOperator(firstNum, secNum, operator) {
             return firstNum / secNum;
     }
 }
-function checkValidNum(num) {
+
+function checkValidNum(num: string): boolean {
     return !isNaN(parseInt(num));
 }
-function checkValidOperator(operator) {
+
+function checkValidOperator(operator: string): boolean {
     if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
+
 }
+
 calculator();
